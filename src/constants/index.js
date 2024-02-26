554fd32058b3e8847bb046b77c45c17d50728a0c
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2024-02-27 00:26:48 +0100

    Final touches

--- src/constants/index.js
+++ src/constants/index.js
@@ -40,10 +40,7 @@ export const speakers = [
   {
     imgURL: ademiku,
     name: 'Ademiku Adeleye',
-    about: `Ademiku Adeleye is a mentor at UK’s Diana Award and IntoUniversity, where he mentors teenagers in United Kingdom to attain growth in their academic, social, and future career. He has over 6-year experience in volunteering and leadership; Recognized by United Nation Association-NG as Top 3 Youth Leader (Nigeria); organized project with impact reaching over 15,000 youths in 13 states in a project in Nigeria. <br>
-    Awarded the Erasmus Mundus, Mastercard Foundation, and finalist of the Mandela Rhodes Foundation Scholarship all in 2023. Currently, He is the Post Graduate School Representative at the University of Edinburgh, UK where he studies Food Security. He aims to improve on the exponential food crisis in developing communities and the world, and demonstrate leadership capacity to help the marginalized community. He is an embodiment of resilience, great enthusiasm, and leadership.
-  <br>
-He is a member of several impactful global organizations – United Nation Academic Impact and Millennium Campus Network; Helping Hand International, ForbesBLK, International Food and Agribusiness Management Association, Aspire Institute, Leap Africa etc. He leverages his communication, administrative and collaboration skills for community development, sharing global opportunities, inspiring youths - most importantly for women and the marginalized community, and promote the United Nations SDGs.`,
+    about: `Ademiku Adeleye is a mentor at UK’s Diana Award and IntoUniversity, where he mentors teenagers in United Kingdom to attain growth in their academic, social, and future career. He has over 6-year experience in volunteering and leadership; Recognized by United Nation Association-NG as Top 3 Youth Leader (Nigeria); organized project with impact reaching over 15,000 youths in 13 states in a project in Nigeria. <br>`,
   },
   {
     imgURL: pstSegun,
@@ -54,66 +51,39 @@ He is a member of several impactful global organizations – United Nation Acade
   {
     imgURL: amarachi,
     name: 'Amarachukwu Nwokororo',
-    about: `Amarachukwu Nwokokoro is a social development advocate, Lawyer , author & content creator who values dedication, excellence and impact. As a dedicated  visionary, Creating Impact & Inspiring Passionate Minds is what drives her daily. <br>She is a passionate business owner, founder and creator dedicated to making an impact in the world and most especially Africa. These passions have motivated her to become a Nation-builder and create several platforms such as IRUKA, NaijaWave, Lilies Africa and plenty more. These platforms form as tools for her to create the transformations she desires in the world falling under the Sustainable Development Goals (SDG) , Quality Education (4) & Peace, Justice and Strong Institutions (16).`,
+    about: `Amarachukwu Nwokokoro is a social development advocate, Lawyer , author & content creator who values dedication, excellence and impact. These passions have motivated her to become a Nation-builder and create several platforms such as IRUKA, NaijaWave, Lilies Africa and plenty more. These platforms form as tools for her to create the transformations she desires in the world falling under the Sustainable Development Goals (SDG) , Quality Education (4) & Peace, Justice and Strong Institutions (16).`,
   },
   {
     imgURL: rubie,
     name: 'Rubie Ibuola',
     about: `Rubie is fortunate to belong to a supportive Christian community that has contributed immeasurably to bolstering her experience of walking with the Holy Spirit.<br>
-She is a product of many graces and has a mantra that says; “anywhere you see me, I am there for the gospel”, whether on a panel with other researchers, an invited speaker at an invent, or at a social gathering. She believes she is there because the gospel will prevail through her.
-She is a bible teacher, published author, counsellor, convener of Foch woman & Techrubies, podcaster and writer.
-She works as a software engineering lecturer and researcher in educational technology.
-<br>
-At the end of her life, like Oral Roberts, Rubie wants herself and everyone she encounters to say “I lived for the gospel and died empty” and like Apostle Paul, boldly declare that “I have fought the good fight, I have finished the race, I have kept the faith”. `,
+She is a product of many graces and has a mantra that says; “anywhere you see me, I am there for the gospel”, whether on a panel with other researchers, an invited speaker at an invent, or at a social gathering. She believes she is there because the gospel will prevail through her.. `,
   },
   {
     imgURL: mona,
     name: 'Mona Adaba',
-    about: `Mona Adaba is a licensed financial professional also known as the Cash Value Queen. She's actively engaged in helping you make a good plan for your money and your family's future.<br>
+    about: `Mona Adaba is a licensed financial professional also known as the Cash Value Queen.<br>
 Her plan is not just about you being safe financially; it's about using smart tools to make life better, like:
 <li>Saving money for your kids' education</li>
 <li>Making more money over time</li>
 <li>Protecting your income if something goes wrong</li>
 <li>Making things easier for your family legally and paying fewer taxes</li>
-<li>Having a calm and happy retirement</li>
 
-She's all into sharing some valuable information about money that you might not know about.
-<br>
-Talking about money can be confusing, especially for African families. That's why she's here to explain things in a way that makes sense and helps you and your family.
-<br>
 She has been helping people with money for more than 3 years, and she has done a great job, earning the title Cash Value Queen. `,
   },
   {
     imgURL: rotimi,
     name: 'Boluwatife Rotimi',
-    about: `Boluwatife Rotimi, is the founder of Techies on LinkedIn, a vibrant community uniting tech enthusiasts worldwide, She is a visionary change maker with an insatiable appetite for tech and social impact.
+    about: `Boluwatife Rotimi, is the founder of Techies on LinkedIn, a vibrant community uniting tech enthusiasts worldwide.
  <br>
-As a personal branding expert and influential figure on LinkedIn, she has empowered countless individuals to enhance their online presence, and has over 25,000 followers on the platform. Boluwatife's impact has been recognized through numerous accolades, including being named among the top 25 Nigerian creators on LinkedIn by Favicon, as well as ranking in the top 100 changemakers in West Africa. Notably, she was honored with the prestigious Creator of the Year award at LinkedIn Local Bootcon and won the most impactful GenZ of the year 2023 by TIBA.
- <br>
-A fervent advocate for talent optimization, Boluwatife champions growth and development, particularly among young people, regardless of their backgrounds. `,
+She has empowered countless individuals to enhance their online presence, and has over 25,000 followers on the platform. Boluwatife's impact has been recognized through numerous accolades, including being named among the top 25 Nigerian creators on LinkedIn by Favicon, as well as ranking in the top 100 changemakers in West Africa.
+ `,
   },
   {
     imgURL: doyinmola,
     name: 'Doyinmola Paul Oyewusi',
     about: `Born in the vibrant Nigerian community of Ondo, Doyinmola felt a deep calling to contribute positively to the world. Seeing the importance of sustainable African culture and crafts ignited a passion within her. At 19, she embarked on a mission to inspire and empower young people after discovering the centuries-old tradition of Adire, an African indigenous textile making technique, and, with the alarming environmental impact of disposed clothing, she saw a unique opportunity to create a cleaner, greener and healthier environment.
   <br>
-Doyinmola founded Dhoney Adire and Crafts Nigeria, a company that gives new life into old clothes through the art of Adire revamp and conscious fashion. This approach promotes cultural preservation while tackling a global crisis. By building a strong community and fostering a sense of ownership, she has inspired over 2500 Africans to actively participate in sustainable fashion initiatives, creating a lasting impact
-<br>
-Asides being an Entrepreneur, she is a dedicated educator, she actively educates as a Sustainable Fashion Trainer through her articles and social media content creation
-
-Some of Doyinmola’s Awards & Achievements:
-<br>
-♻ Certified United People Global Sustainability Leader, 2023
-<br>
-♻ Creative Business Cup Finalist, Enterprise Development Centre, Lagos Business School, 2021
-<br>
-♻ Future Forward Female (Cultural Ambassador Category), 2023
-<br>
-♻ EEFP Grant Winner: Awarded by the Nigerian Export Promotion Council (NEPC) for her sustainable and impact-driven business, Dhoney Adire and Crafts Nigeria, 2022
-<br>
-♻ 25under25 Award Winner (Fashion Category): Awarded by SME 100 Africa, 2021
-<br>
-♻ Young Entrepreneur of the Year Nominee, ELOY Awards, 2021
  `,
   },
 
@@ -122,9 +92,7 @@ Some of Doyinmola’s Awards & Achievements:
     name: 'Elijah Mucungunzi',
     about: `Elijah Mucunguzi is a seasoned Investment Consultant based in Uganda, boasting an impressive track record in investment advisory, Financial Literacy, sales, and marketing. Elijah has been instrumental in delivering top-notch investment advisory services and executing high-investment programs for various industries in Uganda.
 <br>
-Collaborating with numerous prominent NGOs, SMEs and Companies, Elijah's strategic initiatives consistently drive financial awareness and open doors to lucrative investment opportunities. His expertise lies in investment advisory for financial assets, account management, customer service, and financial literacy.
-<br>
-Specialising in investment advisory for financial assets, account management, customer service, and financial literacy, Elijah offers invaluable insights and experiences to individuals, NGOs, and business owners. His mission is to guide you in making informed investment decisions across different financial assets. `,
+Collaborating with numerous prominent NGOs, SMEs and Companies, Elijah's strategic initiatives consistently drive financial awareness and open doors to lucrative investment opportunities. His expertise lies in investment advisory for financial assets, account management, customer service, and financial literacy. `,
   },
 ]
 
